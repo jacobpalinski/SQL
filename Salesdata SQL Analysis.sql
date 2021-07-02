accfc0e12b330272eb95dbd8f22fb09b904c5467
jacob <jacob.palinski@outlook.com>
2021-07-02 20:54:43 +0800

    Part 2

--- Salesdata SQL Analysis.sql
+++ Salesdata SQL Analysis.sql
@@ -67,12 +67,90 @@ Order By AVG(Freight) Desc
 Select Top 3 ShipCountry,
 AVG(Freight) as AverageFreight
 From Orders
-Where OrderDate>DATEADD(year,-1,MAX(OrderDate))
+Where OrderDate>DATEADD(year,-1,(Select MAX(OrderDate) From Orders))
 Group By ShipCountry
 Order By AVG(Freight) Desc
 
-select * from orders
+--List of Inventory
+Select Orders.EmployeeID, 
+LastName, 
+Orders.OrderID, 
+ProductName, 
+Quantity
+from Orders
+Inner Join
+Employees 
+on Orders.EmployeeID=Employees.EmployeeID
+Inner Join
+OrderDetails
+on Orders.OrderID=OrderDetails.OrderID
+Inner Join 
+Products
+on Products.ProductID=OrderDetails.ProductID
 
+--Customers with No Orders
+Select Customers.CustomerID as Customers_CustomerID,
+Orders.CustomerID as Orders_CustomerID
+From Customers
+Left Join
+Orders
+on Customers.CustomerID=Orders.CustomerID
+where Orders.CustomerID is null
+
+--Highest Value Customers Discounted and Non Discounted Order Values
+--(Highest Value defined as Customers with Total Orders >15000 Non Discounted in the Last Year)
+Select Customers.CustomerID,
+CompanyName, 
+SUM(Quantity*UnitPrice) as TotalWithoutDiscount, 
+SUM((1-Discount)*(Quantity*UnitPrice)) as TotalWithDiscount
+from Customers
+Inner Join 
+Orders
+on Customers.CustomerID=Orders.CustomerID
+Inner Join
+OrderDetails
+on Orders.OrderID=OrderDetails.OrderID
+Where OrderDate>DATEADD(year,-1,(Select MAX(OrderDate) From Orders))
+Group By Customers.CustomerID, CompanyName
+Having SUM(UnitPrice*Quantity)>15000
+Order By TotalWithDiscount desc
+
+--Month End Orders
+Select EmployeeID,
+OrderID 
+From Orders
+Where DATEDIFF(d,OrderDate,EOMONTH(Orderdate))=0
+Order By EmployeeID, OrderID asc
+
+--Top 10 Orders with Most Line Items
+Select top 10 OrderID, 
+COUNT(OrderID) as TotalOrderDetails
+From OrderDetails
+Group By OrderID
+Order By TotalOrderDetails desc
+
+--Late Orders
+Select OrderID,
+OrderDate,
+RequiredDate,
+ShippedDate
+From Orders
+Where ShippedDate>RequiredDate
+
+--Salespeople with most late orders
+Select Orders.EmployeeID, 
+LastName,
+COUNT(Orders.EmployeeID) as TotalLateOrders
+From Orders
+INNER JOIN
+Employees
+on Orders.EmployeeID=Employees.EmployeeID
+Where ShippedDate>RequiredDate
+Group By Orders.EmployeeID, LastName
+Order By TotalLateOrders Desc
+
+
+sp_tables
 
 
 
