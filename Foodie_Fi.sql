164bcaa76c6acab4df117717ee471d2f322cb21a
Jacob Palinski <64313786+jacobpalinski@users.noreply.github.com>
2021-08-22 09:35:20 +0800

    Churn AfterTrial

--- Foodie_Fi.sql
+++ Foodie_Fi.sql
@@ -25,11 +25,39 @@ WHERE YEAR(start_date)>2020
 GROUP BY plan_name 
 
 --- What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
-select * from subscriptions
-select * from plans
-
-SELECT COUNT(subscriptions.customer_id),
-1.0*(SELECT COUNT(distinct customer_id) FROM subscriptions)/COUNT(subscriptions.customer_id)
+SELECT COUNT(subscriptions.customer_id) as churn_customer,
+100.0* COUNT(subscriptions.customer_id)/(SELECT COUNT(distinct customer_id) FROM subscriptions) as churn_percentage
 FROM plans
 JOIN
 subscriptions
+on plans.plan_id=subscriptions.plan_id
+WHERE plan_name='churn'
+
+--- How many customers have churned straight after the initial free trial - what percentage is rounded to the nearest whole
+--- number?
+with trialplans as (SELECT customer_id,
+plan_name,
+plan_name1=lead(plan_name,1) over (order by start_date asc)
+FROM plans
+JOIN
+subscriptions
+on plans.plan_id=subscriptions.plan_id)
+
+SELECT COUNT(customer_id) as trialchurn_customer,
+100.0* COUNT(customer_id)/(SELECT COUNT(distinct customer_id) FROM subscriptions) as churn_percentage
+from trialplans
+WHERE plan_name='trial' AND plan_name1='churn'
+
+--- What is the number and percentage of customer plans after their initial free trial?
+SELECT (SELECT COUNT(distinct customer_id) FROM subscriptions WHERE
+plan_id!=0) as aftertrial_customer,
+100.0* COUNT(subscriptions.customer_id)/(SELECT COUNT(distinct customer_id) FROM subscriptions WHERE
+plan_id!=0) as churn_percentage
+FROM plans
+JOIN
+subscriptions
+on plans.plan_id=subscriptions.plan_id
+where plans.plan_id=0
+
+select * from plans
+select * from subscriptions
